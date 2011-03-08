268e32c0fe44feefce5e6c09c2a73146f4012bb1
Alan Grow <alangrow@gmail.com>
2011-03-07 21:37:31 -0500

    minor fixes to unit test suite

--- test.py
+++ test.py
@@ -30,16 +30,17 @@ TEST_BOTH = TEST_UNFLATTEN | TEST_FLATTEN
 
 TRUTH = [
   ("empty", TEST_BOTH, {}, {}),
-#  ("empty key", TEST_BOTH, {'':1}, {'':1}),
+  ("empty key", TEST_BOTH, {'':1}, {'':1}),
   ("depth 2 path x 1", TEST_BOTH, {"a.b":42}, {"a":{"b":42}}),
   ("depth 2 path x 2", TEST_BOTH, {"a.b":42,"c.d":"x"}, {"a":{"b":42},"c":{"d":"x"}}),
   ("depth 2 path x 2, overlap", TEST_BOTH, {"a.b":42,"a.c":"x"}, {"a":{"b":42,"c":"x"}}),
   ("simple list", TEST_BOTH, {"a.0":1,"a.1":2}, {"a":[1,2]}),
   ("sparse list", TEST_BOTH, {"a.0":1,"a.9":10}, {"a":[1,None,None,None,None,None,None,None,None,10]}),
-  ("depth 6", TEST_BOTH, {"a.b.c.d.e":1}, {"a":{"b":{"c":{"d":{"e":1}}}}}),
+  ("deep dict", TEST_BOTH, {"a.b.c.d.e":1}, {"a":{"b":{"c":{"d":{"e":1}}}}}),
   ("list under dict", TEST_BOTH, {"a.b.0":1,"a.b.1":2}, {"a":{"b":[1,2]}}),
   ("dict under list x 1", TEST_BOTH, {"a.0.b":1}, {"a":[{"b":1}]}),
   ("dict under list x 2, overlap", TEST_BOTH, {"a.0.b":1,"a.0.c":2}, {"a":[{"b":1,"c":2}]}),
+  ("deep list", TEST_BOTH, {"a.0.0.0.0":42}, {"a":[[[[42]]]]}),
 ]
 
 
@@ -53,7 +54,7 @@ def main():
     if mode & TEST_FLATTEN:
 
       suite.addTest(FunctionTestCase(
-        name="test %d %s" % (i,name),
+        name="test %d flatten %s" % (i,name),
         func=flatten,
         args=[unflat],
         expected=flat,
@@ -63,7 +64,7 @@ def main():
     if mode & TEST_UNFLATTEN:
 
       suite.addTest(FunctionTestCase(
-        name="test %d %s" % (i,name),
+        name="test %d unflatten %s" % (i,name),
         func=unflatten,
         args=[flat],
         expected=unflat,
