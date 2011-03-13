52854d3309dd6bdf7983231f83d6febc565ae622
Alan Grow <alangrow@gmail.com>
2011-03-13 18:33:15 -0400

    add sdist / pypi info

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension, Command
+"""flattery - fast flattening and unflattening of nested Python data
+
+This library exposes a fast C implementation for flattening and unflattening hierarchical Python data structures. A unit test suite is included. See README.md for usage examples.
+"""
+
+classifiers = """\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Topic :: Text Processing :: General
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: OS Independent
+"""
+
+from distutils.core import setup, Extension
+
+doclines = __doc__.split("\n")
 
 setup(
   name='flattery',
@@ -14,5 +31,13 @@ setup(
     ),
   ],
   packages=['flattery'],
+  maintainer="Alan Grow",
+  maintainer_email="alangrow+flattery@gmail.com",
+  url="https://github.com/acg/python-flattery",
+  license = "https://github.com/acg/python-flattery/blob/master/LICENSE",
+  platforms = ["any"],
+  description = doclines[0],
+  classifiers = filter(None, classifiers.split("\n")),
+  long_description = "\n".join(doclines[2:]),
 )
 
